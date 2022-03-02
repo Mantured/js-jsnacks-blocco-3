6c5b9d7d937fefdd963a367e94f3b955d2bde21f
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-02 15:37:28 +0100

    add bonus 2 -3 , dosent work yet

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -57,3 +57,46 @@ BONUS 2
     Genera N array, ognuno formato da M numeri casuali tra 4 e 4532 che sommati diano almeno L.
     Ogni volta che ne crei uno, stampalo a schermo.
  */
+
+/*     let numberUserN = parseInt(prompt('Inserisci un numero N'));
+    let numberUserM = parseInt(prompt('Inserisci un numero M'));
+    let numberUserL = parseInt(prompt('Inserisci un numero L'));
+
+    while (isNaN(numberUserN) && isNaN(numberUserM) && isNaN(numberUserL)) {
+        numberUserN = parseInt(prompt('Inserisci un numero'));
+        numberUserM = parseInt(prompt('Inserisci un numero'));
+        numberUserL = parseInt(prompt('Inserisci un numero'));
+    }
+
+    if (numberUserN && numberUserN >= 100) {
+        numberUserN = 100;
+        numberUserM = 100;
+    }
+
+
+for (let i = 0; i < numberUserN; i++) {
+    let tempArray = [];
+    let sum = 0;
+    let garbage = [];
+
+    while (tempArray.length < numberUserM && sum < numberUserL) {
+        let randomNumber = (Math.floor(Math.random() * 4528) + 4);
+        tempArray.push(randomNumber);
+
+        if (tempArray.length === numberUserM) {
+            for (let x = 0; x > numberUserM; x++) {
+                sum += tempArray[x];
+            }
+            if (sum >  numberUserL) garbage = [];
+        }
+        console.log(tempArray);
+    }
+    console.log(sum);
+    console.log(garbage);
+} */
+
+/* # BONUS 3
+
+    Fai inserire tre numeri, che chiameremo N & M & L, all'utente.
+    Genera N array, ognuno formato da M numeri casuali tra 4 e 4532 che sommati diano al più L.
+    Ogni volta che ne crei uno, stampalo a schermo. */
